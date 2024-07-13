15dc513dbd68ec2f3c2a34400e2fd866f4eef33d
doubleyoushinamide <olusholasalau@gmail.com>
2024-07-13 11:37:43 +0100

    Added mac and ip addresses

--- user_capture/capture_users.sh
+++ user_capture/capture_users.sh
@@ -12,6 +12,29 @@ NOTE
 # 1. Get list of all IP addresses on the same network as you are
 ips=$(sudo arp-scan --localnet | grep -o -E '([0-9]{1,3}\.){3}[0-9]{1,3}')
 
-# 2. Display the list of IP addresses
-echo "IP addresses on the network:"
-echo "$ips"
+# 2. Get list of all MAC addresses corresponding to the IP addresses
+mac_addr=$(sudo arp-scan --localnet | grep -o -E '([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}')
+
+# 3. Display the list of IP addresses and MAC addresses
+echo "IP addresses and MAC addresses on the network:"
+# 4. Splitting results into arrays using a loop
+ip_array=()
+mac_array=()
+
+# I would have used `readarray` if this script was run on a linux/Ubuntu machine
+# Instead, I will split ips and macs into seperate arrays and join them using a loop
+
+# 5. Split IP addresses into array
+while IFS= read -r line; do
+    ip_array+=("$line")
+done <<< "$ips"
+
+# Split MAC addresses into array
+while IFS= read -r line; do
+    mac_array+=("$line")
+done <<< "$mac_addr"
+
+# iterating through the arrays and join the IP and MAC addresses side by side
+for (( i=0; i<${#ip_array[@]}; i++ )); do
+    echo "${ip_array[i]}   ${mac_array[i]}"
+done
