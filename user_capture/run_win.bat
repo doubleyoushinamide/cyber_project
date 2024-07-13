6f98757fcc2d35de262a7822cfbabd623c828bbc
doubleyoushinamide <olusholasalau@gmail.com>
2024-07-13 12:39:47 +0100

    added windows functionality

---
+++ user_capture/run_win.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM or remark: - path to your Git Bash installation
+REM If you installed Git properlly by default your path should be at `C:\Program Files\Git\bin\bash.exe`
+set GIT_BASH_PATH = C:\Program Files\Git\bin\bash.exe
+
+REM chanell the path to our capture_users.sh
+set SCRIPT_PATH=%~dp0capture_users.sh
+
+"%GIT_BASH_PATH%" --login -i -c "bash '%SCRIPT_PATH%'"
