5aa79fc2d7b59590d7370f46c032ef5102b41157
doubleyoushinamide <olusholasalau@gmail.com>
2024-07-13 12:24:48 +0100

    straight forward with the run_net

--- user_capture/run_net.sh
+++ user_capture/run_net.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-script="./capture_users.sh"  # Adjust the script name/path as needed
-
-# Ensure script is executable
-chmod +x "$script"
-
-# Run the script with sh
-sh "$script"
+sh "./capture_users.sh"  # Adjust the script name/path as needed
